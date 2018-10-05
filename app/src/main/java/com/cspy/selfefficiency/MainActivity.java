9f19e34aee229dd460b16e052e9e99ab4fd14006
CSpy <cspy2117@163.com>
2018-10-05 22:41:08 +0800

    第一次初始化

--- app/src/main/java/com/cspy/selfefficiency/MainActivity.java
+++ app/src/main/java/com/cspy/selfefficiency/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cspy.selfefficiency;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
 
     }
 }
